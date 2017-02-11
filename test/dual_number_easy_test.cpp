0a0961c588bed1117cf66594bf8ea38d3122eb87
DESKTOP-CNI7H45\yussa <loligothick@gmail.com>
2017-02-11 11:34:00 +0900

    intervalだけでテスト

--- test/dual_number_easy_test.cpp
+++ test/dual_number_easy_test.cpp
@@ -1,83 +1,83 @@
 #include <iostream>
-#include "../math/dual_number.hpp"
+//#include "../math/dual_number.hpp"
 #include "../math/interval.hpp"
 
 int main()
 try
 {
-  using namespace cranberries::math;
+  // using namespace cranberries::math;
   using cranberries::interval_lib::interval;
-  using namespace cranberries::math::literals;
+  // using namespace cranberries::math::literals;
   using std::cout;
   using std::endl;
 
-  auto x = make_dual(1.0, 1.0);
-  auto y = make_dual(0.5,1.0);
-  auto z = make_dual(-1.0,1.0);
+  // auto x = make_dual(1.0, 1.0);
+  // auto y = make_dual(0.5,1.0);
+  // auto z = make_dual(-1.0,1.0);
 
-  cout << dual_number<>{} << endl;
-  cout << make_dual(1.0) << endl;
-  cout << make_dual(2.0, 1.0) << endl;
-  cout << 2.0_dual << endl;
+  // cout << dual_number<>{} << endl;
+  // cout << make_dual(1.0) << endl;
+  // cout << make_dual(2.0, 1.0) << endl;
+  // cout << 2.0_dual << endl;
   
-  auto a = dual_number<interval<>>{ interval<>{1,3}, interval<>{1} };
-  cout << sin(a) << endl;
+  // auto a = dual_number<interval<>>{ interval<>{1,3}, interval<>{1} };
+  // cout << sin(a) << endl;
 
-  auto v1 = x;
-  cout << (v1 += 1) << endl;
-  cout << (v1 += v1) << endl;
-  cout << x + x << endl;
-  cout << x + 1 << endl;
-  cout << 1 + x << endl;
+  // auto v1 = x;
+  // cout << (v1 += 1) << endl;
+  // cout << (v1 += v1) << endl;
+  // cout << x + x << endl;
+  // cout << x + 1 << endl;
+  // cout << 1 + x << endl;
 
-  auto v2 = x;
-  cout << (v2 -= 1) << endl;
-  cout << (v2 -= v2) << endl;
-  cout << x - x << endl;
-  cout << x - 1 << endl;
-  cout << 1 - x << endl;
+  // auto v2 = x;
+  // cout << (v2 -= 1) << endl;
+  // cout << (v2 -= v2) << endl;
+  // cout << x - x << endl;
+  // cout << x - 1 << endl;
+  // cout << 1 - x << endl;
 
-  auto v3 = x;
-  cout << (v3 *= 1) << endl;
-  cout << (v3 *= v3) << endl;
-  cout << x * x << endl;
-  cout << 1 * x << endl;
-  cout << x * 1 << endl;
+  // auto v3 = x;
+  // cout << (v3 *= 1) << endl;
+  // cout << (v3 *= v3) << endl;
+  // cout << x * x << endl;
+  // cout << 1 * x << endl;
+  // cout << x * 1 << endl;
 
-  auto v4 = x;
-  cout << (v4 /= 1) << endl;
-  cout << (v4 /= v4) << endl;
-  cout << x / x << endl;
-  cout << 1 / x << endl;
-  cout << x / 1 << endl;
+  // auto v4 = x;
+  // cout << (v4 /= 1) << endl;
+  // cout << (v4 /= v4) << endl;
+  // cout << x / x << endl;
+  // cout << 1 / x << endl;
+  // cout << x / 1 << endl;
 
-  cout << sin(x) << endl;
-  cout << cos(x) << endl;
-  cout << tan(x) << endl;
-  cout << sec(x) << endl;
-  cout << csc(x) << endl;
-  cout << cot(x) << endl;
-  cout << asin(y) << endl;
-  cout << acos(y) << endl;
-  cout << atan(y) << endl;
-  cout << sinh(x) << endl;
-  cout << cosh(x) << endl;
-  cout << tanh(x) << endl;
-  cout << sech(x) << endl;
-  cout << csch(x) << endl;
-  cout << coth(x) << endl;
-  cout << asinh(x) << endl;
-  cout << acosh(make_dual(1.2,1.0)) << endl;
-  cout << atanh(y) << endl;
-  cout << asech(y) << endl;
-  cout << acsch(make_dual(-2.0,1.0)) << endl;
-  cout << acoth(make_dual(-2.0, 1.0)) << endl;
-  cout << abs(z) << endl;
-  cout << log(x) << endl;
-  cout << exp(x) << endl;
-  cout << pow(x,2) << endl;
-  cout << sqrt(y) << endl;
-  cout << cbrt(y) << endl;
+  // cout << sin(x) << endl;
+  // cout << cos(x) << endl;
+  // cout << tan(x) << endl;
+  // cout << sec(x) << endl;
+  // cout << csc(x) << endl;
+  // cout << cot(x) << endl;
+  // cout << asin(y) << endl;
+  // cout << acos(y) << endl;
+  // cout << atan(y) << endl;
+  // cout << sinh(x) << endl;
+  // cout << cosh(x) << endl;
+  // cout << tanh(x) << endl;
+  // cout << sech(x) << endl;
+  // cout << csch(x) << endl;
+  // cout << coth(x) << endl;
+  // cout << asinh(x) << endl;
+  // cout << acosh(make_dual(1.2,1.0)) << endl;
+  // cout << atanh(y) << endl;
+  // cout << asech(y) << endl;
+  // cout << acsch(make_dual(-2.0,1.0)) << endl;
+  // cout << acoth(make_dual(-2.0, 1.0)) << endl;
+  // cout << abs(z) << endl;
+  // cout << log(x) << endl;
+  // cout << exp(x) << endl;
+  // cout << pow(x,2) << endl;
+  // cout << sqrt(y) << endl;
+  // cout << cbrt(y) << endl;
 }
 catch (const std::exception& e)
 {
