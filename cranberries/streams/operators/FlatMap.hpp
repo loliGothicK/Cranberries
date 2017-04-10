ce0aa1f1f39f2c59b7ee4480954ea84faa3b9aef
enumhack-BoW <loligothick@gmail.com>
2017-04-10 09:17:48 +0900

    hoge

---
+++ cranberries/streams/operators/FlatMap.hpp
@@ -0,0 +1,139 @@
+#ifndef CRANBERRIES_STREAMS_OPERATORS_FLATMAP_HPP
+#define CRANBERRIES_STREAMS_OPERATORS_FLATMAP_HPP
+#include <utility>
+#include <type_traits>
+#include <algorithm>
+#include "../utility.hpp"
+#include "../workaround.hpp"
+
+namespace cranberries {
+namespace streams {
+namespace operators {
+
+  template <
+    typename UnaryOp
+  >
+  class FlatMap
+    : private cranberries_magic::LazyOperationModuleBase
+  {
+  public:
+    FlatMap( UnaryOp op ) noexcept
+      : op_{ std::forward<UnaryOp>( op ) }
+    {}
+
+    template <
+      typename Stream,
+      typename T
+    >
+    void
+    push_tuple
+    (
+      Stream&& stream_,
+      T&& arg
+    )
+      noexcept
+    {
+      stream_.emplace_back(std::move(arg));
+      return;
+    }
+
+    template <
+      typename Stream,
+      typename Head,
+      typename ...Tail
+    >
+    void
+    push_tuple(
+      Stream&& stream_,
+      Head&& head,
+      Tail&& ...tail
+    )
+      noexcept
+    {
+      return stream_.emplace_back( std::move( head ) ), push_tuple( std::forward<Stream>( stream_ ), std::forward<Tail>( tail )... );
+    }
+
+
+    template <
+      typename T,
+      typename Stream,
+      std::enable_if_t<is_tuple_v<std::decay_t<T>>,std::nullptr_t> = nullptr
+    >
+    void
+    push
+    (
+      Stream&& stream_,
+      T&& proj
+    )
+      noexcept
+    {
+      cranberries::apply(
+        [&]( auto&& ...args ) {
+        push_tuple( std::forward<Stream>( stream_ ), std::forward<decltype( args )>( args )... ); 
+      }, std::forward<T>( proj ) );
+    }
+
+    template <
+      typename Stream,
+      typename Range,
+      std::enable_if_t<
+        is_range_v<std::decay_t<Range>>,
+        std::nullptr_t
+      > = nullptr
+    >
+    void
+    push
+    (
+      Stream&& stream_,
+      Range&& range_
+    )
+      noexcept
+    {
+      workaround::front_emplacer<
+        cranberries_magic::is_finite_stream_v<std::decay_t<Range>>
+      >::invoke(std::forward<Stream>(stream_), std::forward<Range>(range_));
+    }
+
+    template <
+      typename Stream
+    >
+    decltype(auto)
+    operator()
+    (
+      Stream&& stream_
+    )
+      noexcept(false)
+    {
+      CRANBERRIES_STREAM_EMPTY_ERROR_THROW_IF( stream_.empty() );
+      auto old = stream_.get();
+      stream_.clear();
+      for (auto&& e : old ) {
+        push( stream_, op_( e ) );
+      }
+      return std::forward<Stream>( stream_ );
+    }
+
+
+    template <
+      typename T
+    >
+    decltype(auto)
+    operator[]
+    (
+      T&& a
+    )
+      noexcept
+    {
+      return op_(a);
+    }
+
+  private:
+    UnaryOp op_;
+  };
+
+} // ! namespace operators
+} // ! namespace streams
+} // ! namespace cranberries
+
+
+#endif
