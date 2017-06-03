8e0554aa1252fae7fabd9ecce14352b64fe061a5
enumhack-win10 <loligothick@gmail.com>
2017-06-04 03:49:42 +0900

    RKF45を追加

--- test/rkf45/test.cpp
+++ test/rkf45/test.cpp
@@ -6,10 +6,9 @@
 int main()
 {
   auto solver = cranberries::make_rkf45([](long double t, long double x) { return x*std::cos(t); })
-      .set_variable_range({ 0,10 })
-      .set_init_value(0)
-      .set_tolerance(1.0E-8L)
-      .set_step_size_range({ 0.001,0.1 });
+    .set_integrate_range({ 0,10 })
+    .set_tolerance(1.0E-4L)
+    .set_step_size_range({ 0.001,0.5 });
 
-  std::cout << std::boolalpha << solver.solve() << std::endl;
+  solver.integrate(1.0L, [](auto t, auto x) { std::cout << t << " " << x << '\n'; });
 }
