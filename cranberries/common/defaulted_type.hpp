4c7511811e10911c83b9bd01c117d779cf9f63ca
enumhack-win10 <loligothick@gmail.com>
2017-08-20 23:12:48 +0900

    stream/forwardの変更漏れを修正

--- cranberries/common/defaulted_type.hpp
+++ cranberries/common/defaulted_type.hpp
@@ -1,6 +1,7 @@
 #ifndef CRANBERRIES_COMMON_DEFAULTED_TYPE_HPP
 #define CRANBERRIES_COMMON_DEFAULTED_TYPE_HPP
-namespace cranberries{
+
+namespace cranberries {
 namespace cranberries_magic {
 
   // Defaluted Tag
