825e58fb59259832c9bc92abe07ee0c08b6284a1
Yuki Wada <loligothick@gmail.com>
2017-09-15 08:35:52 +0900

    Revert "パラレリズム"
    This reverts commit 4e343bc9cc4c7aeece680a0ed9ddd4e143387936.

--- VisualStudio/CranberriesLibrary/CranberriesLibrary.cpp
+++ VisualStudio/CranberriesLibrary/CranberriesLibrary.cpp
@@ -15,7 +15,9 @@ int main() {
   unit_test{"Unit Test Example"}
     << assert::are_equal([] { std::this_thread::sleep_for(std::chrono::seconds(3)); return 2; }, 2)
     << assert::are_equal([] { std::this_thread::sleep_for(std::chrono::seconds(1)); return 2; }, 2)
-    << assert::permutation_execute<2>([](auto a, auto b) { return a + b; },assert::test_skip, 1, 2, 3, 4, 5)
+    << assert::permutation_execute<2>([](auto a, auto b) { return a + b; },
+      assert::test_skip,
+      1, 2, 3, 4, 5)
     << assert::are_equal([] { return 100; }, 100)
     << assert::range_equal([] { return std::array<int, 4>{ {1, 2, 3, 4}}; }, 1, 2, 3)
     << assert::excact_throw<std::runtime_error>([] { throw std::runtime_error{ "error!" }; })
