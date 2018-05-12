18e591e9f7cd60924c89b210ce4a6587a8a7f6a1
enumhack <loligothick@gmail.com>
2018-05-13 06:41:38 +0900

    fix: func_util [- note: latest MSVC(Visual Studio 2017 version 15.7.3) can compile first version func_util (by minimal fix). -]

--- cranberries/func_util/apply_pipe.hpp
+++ cranberries/func_util/apply_pipe.hpp
@@ -8,11 +8,7 @@
 #ifndef CRANBERRIES_FUNC_UTIL_APPLY_PIPE_HPP
 #define CRANBERRIES_FUNC_UTIL_APPLY_PIPE_HPP
 
-#  ifdef _MSC_VER
-#include "func_util_detail/function_utility_for_gcc_clang.hpp"
-#  else
-#include "func_util_detail/function_utility_for_gcc_clang.hpp"
-#  endif // _MSC_VER
+#include "func_util_detail/function_utility_core.hpp"
 
 namespace cranberries {
 namespace literals {
