6c3aa1d31b97c432f5e06c3010b65b579bf18a40
enumhack <loligothick@gmail.com>
2018-05-13 07:28:49 +0900

    mu

--- VisualStudio/CranberriesLibrary/CranberriesLibrary.cpp
+++ VisualStudio/CranberriesLibrary/CranberriesLibrary.cpp
@@ -1,19 +1,4 @@
 #include <iostream>
-#include <experimental/resumable>
-#include <experimental/coroutine>
-#include <experimental/xutility>
-#include <experimental/filesystem>
-#include <experimental/deque>
-#include <experimental/forward_list>
-#include <experimental/generator>
-#include <experimental/list>
-#include <experimental/map>
-#include <experimental/unordered_map>
-#include <experimental/set>
-#include <experimental/unordered_set>
-#include <experimental/vector>
-#include <execution>
-#include <algorithm>
 #include <cranberries/type_traits.hpp>
 
 int main(){
