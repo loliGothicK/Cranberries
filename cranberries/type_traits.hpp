1caa59863c4764cadcd99b1c5028d71c3ce3b99e
enumhack <loligothick@gmail.com>
2018-05-13 06:13:07 +0900

    add: fix invoke_result, is_invocable_*

--- cranberries/type_traits.hpp
+++ cranberries/type_traits.hpp
@@ -831,78 +831,74 @@ namespace _detail {
 	}
 } // ! namespace _detail
 
+
 namespace _detail {
-  struct failure_t {};
-    
-	template <typename AlwaysVoid, typename, typename...>
-	struct invoke_result_impl { using type = failure_t; };
+	struct failure_t {};
+	template < class T >
+	struct invoke_result_inject { using type = T; };
+	template < >
+	struct invoke_result_inject<failure_t> {};
 
-	template <typename F, typename...Args>
-	struct invoke_result_impl<decltype(void(INVOKE(std::declval<F>(), std::declval<Args>()...))),
-		F, Args...> {
-		using type = decltype(INVOKE(std::declval<F>(), std::declval<Args>()...));
+	template<class AlwaysVoid, class... Types>
+	struct invoke_traits
+	{
+		using type = failure_t;
+		using is_invocable_impl = std::false_type;
+		using is_nothrow_invocable_impl = std::false_type;
+		template<class Rx>
+		using is_invocable_r_impl = std::false_type;
+		template<class Rx>
+		using is_nothrow_invocable_r_impl = std::false_type;
 	};
-    
-  template < class T >
-  struct invoke_result_inject{ using type = T; };
-    
-  template < >
-  struct invoke_result_inject<failure_t> {};
 
-	template <typename AlwaysVoid, typename, typename...>
-	struct nothrow_invoke_result_impl { using type = failure_t; };
+	template<class... Types>
+	struct invoke_traits<
+		::cranberries::void_t<decltype(::cranberries::_detail::INVOKE(std::declval<Types>()...))>,
+		Types...>
+	{
+		using type = decltype(::cranberries::_detail::INVOKE(std::declval<Types>()...));
+		using is_invocable_impl = std::true_type;
+		using is_nothrow_invocable_impl = ::cranberries::bool_constant<noexcept(::cranberries::_detail::INVOKE(std::declval<Types>()...))>;
+		template<class Rx>
+		using is_invocable_r_impl = ::cranberries::disjunction<std::is_void<Rx>, std::is_convertible<type, Rx>>;
+		template<class Rx>
+		using is_nothrow_invocable_r_impl = ::cranberries::disjunction<
+			is_nothrow_invocable_impl,
+			::cranberries::disjunction<std::is_void<Rx>, ::cranberries::is_nothrow_convertible<type, Rx>>>;
+	};
 
-	template <class F, class... Args>
-	struct nothrow_invoke_result_impl< std::enable_if_t<noexcept(INVOKE(std::declval<F>(), std::declval<Args>()...)) >,
-		F, Args...>: invoke_result_impl<void, F, Args...> {};
-        
-  template < class R, class F, class... ArgTypes >
-	struct is_invocable_impl
-    : disjunction<
-				bool_constant<std::is_void<R>::value && !std::is_same<typename invoke_result_impl<void, F, ArgTypes...>::type, failure_t>::value>,
-        std::is_convertible<typename invoke_result_impl<void, F, ArgTypes...>::type, R>
-    >{};
-    
-  template < class R, class F, class... ArgTypes >
-	struct is_nothrow_invocable_impl
-    : disjunction<
-        bool_constant<std::is_void<R>::value && !std::is_same<typename nothrow_invoke_result_impl<void, F, ArgTypes...>::type, failure_t>::value>,
-        std::is_convertible<typename nothrow_invoke_result_impl<void, F, ArgTypes...>::type, R>
-    >{};
 }
 
-	template <class F, class... ArgTypes>
-	struct invoke_result : _detail::invoke_result_inject<typename _detail::invoke_result_impl<void, F, ArgTypes...>::type> {};
+template <class F, class... ArgTypes>
+struct invoke_result : _detail::invoke_result_inject<typename _detail::invoke_traits<void, F, ArgTypes...>::type> {};
 
-	template < class F, class... ArgTypes >
-	struct is_invocable : _detail::is_invocable_impl<void, F, ArgTypes...> {};
+template < class F, class... ArgTypes >
+struct is_invocable : _detail::invoke_traits<void, F, ArgTypes...>::is_invocable_impl {};
 
-	template < class R, class F, class... ArgTypes >
-	struct is_invocable_r : _detail::is_invocable_impl<R, F, ArgTypes...> {};
+template < class R, class F, class... ArgTypes >
+struct is_invocable_r : _detail::invoke_traits<void, F, ArgTypes...>:: template is_invocable_r_impl<R> {};
 
-	template < class F, class... ArgTypes >
-	struct is_nothrow_invocable : _detail::is_nothrow_invocable_impl<void, F, ArgTypes...> {};
+template < class F, class... ArgTypes >
+struct is_nothrow_invocable : _detail::invoke_traits<void, F, ArgTypes...>::is_nothrow_invocable_impl {};
 
-	template < class R, class F, class... ArgTypes >
-	struct is_nothrow_invocable_r : _detail::is_nothrow_invocable_impl<R, F, ArgTypes...> {};
+template < class R, class F, class... ArgTypes >
+struct is_nothrow_invocable_r : _detail::invoke_traits<void, F, ArgTypes...>:: template is_nothrow_invocable_r_impl<R> {};
 
 
+template <class F, class... ArgTypes>
+using invoke_result_t = typename invoke_result<F, ArgTypes...>::type;
 
-	template <class F, class... ArgTypes>
-	using invoke_result_t = typename invoke_result<F, ArgTypes...>::type;
+template < class F, class... ArgTypes >
+constexpr bool is_invocable_v = is_invocable<F, ArgTypes...>::value;
 
-	template < class F, class... ArgTypes >
-	constexpr bool is_invocable_v = is_invocable<F, ArgTypes...>::value;
+template < class R, class F, class... ArgTypes >
+constexpr bool is_invocable_r_v = is_invocable_r<R, F, ArgTypes...>::value;
 
-	template < class R, class F, class... ArgTypes >
-	constexpr bool is_invocable_r_v = is_invocable_r<R, F, ArgTypes...>::value;
-    
-    template < class F, class... ArgTypes >
-	constexpr bool is_nothrow_invocable_v = is_nothrow_invocable<F, ArgTypes...>::value;
+template < class F, class... ArgTypes >
+constexpr bool is_nothrow_invocable_v = is_nothrow_invocable<F, ArgTypes...>::value;
 
-	template < class R, class F, class... ArgTypes >
-	constexpr bool is_nothrow_invocable_r_v = is_nothrow_invocable_r<R, F, ArgTypes...>::value;
-
+template < class R, class F, class... ArgTypes >
+constexpr bool is_nothrow_invocable_r_v = is_nothrow_invocable_r<R, F, ArgTypes...>::value;
 
 }
 
